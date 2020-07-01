e2a099b4fd0b7cae245dd65c108ce694f56e7368
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-07-01 11:31:15 +0200

    Updates

---
+++ precache-manifest.91fa35bbe6f8e16c54ecfb28073274ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edded769ced5aea8e97a7e4166f40a69",
+    "url": "/Third-React-App/index.html"
+  },
+  {
+    "revision": "bea137919bbf588f43e4",
+    "url": "/Third-React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/Third-React-App/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Third-React-App/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea137919bbf588f43e4",
+    "url": "/Third-React-App/static/js/main.d0c4e632.chunk.js"
+  },
+  {
+    "revision": "7250b611a8ce1af91bdf",
+    "url": "/Third-React-App/static/js/runtime-main.2eb0a6cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Third-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
